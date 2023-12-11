2bf17a83899bde8f1952d8004db0bd8096ff4ae8
Pallav Sharma <pallavsharma0202@gmail.com>
2023-12-11 22:56:26 +0530

    addind authentication and navbar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,26 @@
-import { NextAuthOptions } from "next-auth"
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth"
+import {prisma} from "./db"
+import {PrismaAdapter} from "@next-auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+
+declare module "next-auth" {
+    interface Session extends DefaultSession {
+        user: {
+            id: string;
+            credits: number;
+        } & DefaultSession["user"]
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id: string;
+        credits: number;
+    }
+}
+
+
+
 export const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt'
@@ -10,6 +32,33 @@ export const authOptions: NextAuthOptions = {
                     email: token.email
                 }
             })
+            if(db_user) {
+                token.id = db_user.id
+                token.credits = db_user.credits
+            }
+            return token
+        },
+        session: ({session, token}) => {
+            if(token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.image = token.picture
+                session.user.email = token.email
+                session.user.credits = token.credits
+            }
+            return session
         }
-    }
+    },
+    secret: process.env.NEXTAUTH_SECRET as string,
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        })
+    ]
+}
+
+export const getAuthSession = () => {
+    return getServerSession(authOptions)
 }
